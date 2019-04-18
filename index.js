832b1e239901339baa6cfe03eb7406593981dd7d
A. Kovalchuk <a.kovalchuk@lucky-labs.com>
2019-04-18 10:39:04 +0300

    Add index file

---
+++ index.js
@@ -0,0 +1,35 @@
+export default (store, router, params = []) => {
+  router.onReady(route => {
+    // Move params from route to state
+    const paramsForPush = {};
+
+    params.forEach(param => {
+      const urlParam = param.as || param.name;
+
+      if (route.query.hasOwnProperty(urlParam)) {
+        store.commit(param.mutation, route.query[urlParam]);
+      } else {
+        paramsForPush[urlParam] = store.state[param.name];
+      }
+    });
+
+    router.push({
+      query: {
+        ...route.query,
+        ...paramsForPush
+      }
+    });
+  });
+
+  // Watch state
+  params.forEach(param => {
+    let urlParam = param.as || param.name;
+
+    store.watch(
+      (state, getters) => state[param.name] || getters[param.name],
+      newValue => {
+        router.push({ query: { ...router.currentRoute.query, [urlParam]: newValue } });
+      }
+    );
+  });
+};
